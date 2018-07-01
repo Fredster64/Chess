6bc3fdd13df1cae6c57c151d763a808984c7145b
Sach Patel <sachin3796@gmail.com>
2018-07-01 21:49:21 +0100

    added git friendly comments

--- chess.cpp
+++ chess.cpp
@@ -1,3 +1,11 @@
+/*****************************************/
+/* "chess.cpp", and its relevant header  */
+/* file, "chess.h", were created by the  */
+/* author, Sach Patel. They were written */
+/* using C++11 rules and conventions.    */
+/*****************************************/
+
+
 /* Includes */
 #include <iostream>
 #include <string>
@@ -21,6 +29,7 @@
 /* Main Function */
 int main (void) {
     
+    // Add the Players
     chess::Player_One p1;
     chess::Player_Two p2;
     
@@ -28,7 +37,7 @@ int main (void) {
     bool p1_is_white;
     
     char state;
-    
+    // Ask the user if they want to play a computer or a friend
     do {
         std::cout << "Would you like to play against a Human or the Computer? (h/c)" << std::endl;
         std::cin >> state;
@@ -36,9 +45,7 @@ int main (void) {
         if (state == 'c') { p2_is_comp = true;  } else
         { std::cout << "Sorry, I didn't understand." << std::endl; }
     } while (state != 'h' && state != 'c');
-    
-    p2.is_computer (p2_is_comp);
-    
+    // Ask the user which colour Player 1 should be
     do {
         std::cout << "Is Player 1 playing as White or Black? (w/b)" << std::endl;
         std::cin >> state;
@@ -47,16 +54,23 @@ int main (void) {
         { std::cout << "Sorry, I didn't understand." << std::endl; }
     } while (state != 'w' && state != 'b');
     
-    p1.is_white ( p1_is_white);
-    p2.is_white (!p1_is_white);
+    // Set the user parameters in the respective classes.
+    p2.is_computer (p2_is_comp);
+    p1.is_white (  p1_is_white);
+    p2.is_white ( !p1_is_white);
     
+    // Define and build the gameboard.
     chess::Board cb;
     cb.board_init();
+    
+    // Debug only: Print the vector of board elements to the console.
     cb.print_board();
     
+    // Define and set up the game engine.
     chess::Game g;
     g.setup(p1, p2);
     
+    // Debug only : Print the vector of pieces to the console.
     p1.print_pieces();
     p2.print_pieces();
     
