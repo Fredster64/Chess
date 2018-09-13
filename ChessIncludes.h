b33161bc4244a38dd236a99d8d8c6aeb96ae3de5
Sach Patel <sachin3796@gmail.com>
2018-09-13 09:20:07 +0100

    made rm_dlt a member function of GameEngine

--- ChessIncludes.h
+++ ChessIncludes.h
@@ -1,13 +1,10 @@
 #ifndef CHESSINCLUDES_H
 #define CHESSINCLUDES_H
 
-#include <memory>
-
 #include "ChessClasses.h" // Class definitions
 
 /* All custom, non-member function prototypes go here. */
 
-void rm_dlt (std::vector<chess::PiecePtr>& v, const chess::pos p2);
 template <typename T>
 bool vec_search (const std::vector<T>& v, const T item);
 
@@ -18,14 +15,6 @@ bool vec_search (const std::vector<T>& v, const T item);
 #include "DerivedPieceMethods.h" // Methods from derived classes of Piece
 
 /* All custom, non-member function definitions go here. */
-
-void rm_dlt (std::vector<chess::PiecePtr>& v, const chess::pos p2) {
-    v.erase (std::remove_if (v.begin(), v.end(), [p2] (chess::PiecePtr piece) -> bool {
-        chess::pos p = piece->check_position();
-        if (p == p2) return true;
-        return false;
-    }), v.end());
-}
 
 template <typename T>
 bool vec_search (const std::vector<T>& v, const T item) {
